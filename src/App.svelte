78f9cac990b8075a6406c574673f9cccb3081f0e
gduarte17 <gduarte17k@gmail.com>
2023-10-11 21:10:01 -0300

    add tictactoe

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,16 @@
 <script>
   export let name;
+
+  import Tictactoe from "./routes/Tictactoe.svelte";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
+  <Tictactoe />
+  <!-- <h1>Hello {name}!</h1>
   <p>
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
     how to build Svelte apps.
-  </p>
-  <div style="display: contents">%sveltekit.body%</div>
+  </p> -->
 </main>
 
 <style>
