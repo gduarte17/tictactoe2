5262e8acc9a5ac8246de4c80e4d641e7dd92a6c5
gduarte17 <gduarte17k@gmail.com>
2023-10-11 21:20:04 -0300

    alpha v0.0.1

--- src/routes/Tictactoe.svelte
+++ src/routes/Tictactoe.svelte
@@ -1,24 +1,14 @@
 <script>
-  // let board = Array(9).fill(null);
-  // let currentPlayer = 'X';
-
-  // function makeMove(index) {
-  //   if (board[index] === null) {
-  //     board[index] = currentPlayer;
-  //     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  //   }
-  // }
-
   // JogoDaVelha.svelte
   import { onMount } from "svelte";
   import { io } from "socket.io-client";
 
-  let board = Array(9).fill(null);
+  let board = Array(9).fill("");
   let currentPlayer = "X";
   const socket = io("http://localhost:3000"); // Substitua pelo seu servidor real
 
   function makeMove(index) {
-    if (board[index] === null) {
+    if (board[index] === "") {
       board[index] = currentPlayer;
       socket.emit("makeMove", index); // Enviar movimento para o servidor
       currentPlayer = currentPlayer === "X" ? "O" : "X";
@@ -39,7 +29,7 @@
   <h1>Jogo da Velha</h1>
   <div class="board">
     {#each board as cell, index (index)}
-      <div class="cell" on:click={() => makeMove(index)}>
+      <div class="cell border-{index}" on:click={() => makeMove(index)}>
         {cell}
       </div>
     {/each}
@@ -47,5 +37,63 @@
 </main>
 
 <style>
-  /* Estilize seu tabuleiro aqui */
+  .cell {
+    width: 75px;
+    height: 75px;
+    text-align: center;
+    font-size: 24px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .game {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: white;
+  }
+
+  .tictactoe-grid {
+    display: grid;
+    grid-template: 32% 32% 32% / 32% 32% 32%;
+    /* gap: 5px; */
+  }
+
+  .border-3,
+  .border-4,
+  .border-5,
+  .border-6,
+  .border-7,
+  .border-8 {
+    border-top: 3px solid #fff;
+  }
+
+  .border-0,
+  .border-1,
+  .border-3,
+  .border-4,
+  .border-6,
+  .border-7 {
+    border-right: 3px solid #fff;
+  }
+
+  .border-0,
+  .border-1,
+  .border-2,
+  .border-3,
+  .border-4,
+  .border-5 {
+    border-bottom: 3px solid #fff;
+  }
+
+  .border-1,
+  .border-2,
+  .border-4,
+  .border-5,
+  .border-7,
+  .border-8 {
+    border-left: 3px solid #fff;
+  }
 </style>
